87c1b2deb41f89734a909ae267d54fb54b8b7b5e
Richard Hunter <richard@richardhunter.co.uk>
2016-06-18 22:27:09 +0100

    deleting old experiments

--- app.js
+++
@@ -1,9 +0,0 @@
-var port = 6464;
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/src'));
-
-function start () {
-	console.log('listening on port:', port);
-}
-app.listen(port, start);
