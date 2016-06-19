5765b94048fe8bb30a4a2f3ef9baf71b29bf7c2e
Richard Hunter <richard@richardhunter.co.uk>
2016-06-19 14:53:40 +0100

    renaming some folders

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var port = 6465;
 var express = require('express');
 var app = express();
 
-app.use('/js', express.static(__dirname + '/with-paging/js'));
-app.use('/css', express.static(__dirname + '/with-paging/css'));
+app.use('/js', express.static(__dirname + '/web/js'));
+app.use('/css', express.static(__dirname + '/web/css'));
 
 app.get('/*', function(req, res){
-  res.sendFile(__dirname + '/with-paging/index.html');
+  res.sendFile(__dirname + '/web/index.html');
 });
 
 function start () {
